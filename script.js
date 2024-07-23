3b7c250bd908cd8435d012e9e68335141da4a545
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-24 00:51:44 +0400

    add styling for looks

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const parentDiv = document.querySelector('.parent-div');
+const slider = document.querySelector('#slider');
+const currentSize = document.querySelector('.current-size')
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement('div');
-    parentDiv.appendChild(div);
-  }
+function createGrid(dimension) {
+	parentDiv.textContent = '';
+	const parentHeight = parentDiv.clientHeight;
+	const parentWidth = parentDiv.clientWidth
+	const childDivHeight = parentHeight / dimension;
+	const childDivWidth = parentWidth / dimension;
+
+
+	for (let i = 0; i < dimension; i++) {
+	  for (let j = 0; j < dimension; j++) {
+	    const div = document.createElement('div');
+			div.style.width = `${childDivWidth}px`;
+			div.style.height = `${childDivHeight}px`;
+	    parentDiv.appendChild(div);
+	  }
+	}
+
+	currentSize.textContent = `${dimension} X ${dimension}`
+	const eachDiv = document.querySelectorAll('.parent-div div');
+
+	eachDiv.forEach(function (el) {
+		el.addEventListener('mouseover', (e) => {
+			e.target.style.backgroundColor = 'blue';
+		});
+	});
 }
 
-const eachDiv = document.querySelectorAll('.parent-div div');
-const slider = document.querySelector('#slider')
+createGrid(16)
 
-eachDiv.forEach(function (el) {
-	el.addEventListener('mouseover', (e) => {
-		e.target.style.backgroundColor = 'blue'
-	});
+slider.addEventListener("input", (el) => {
+	const dimension = Number(el.target.value)
+	createGrid(dimension);
 });
